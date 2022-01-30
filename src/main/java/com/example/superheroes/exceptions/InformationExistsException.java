cb633029bd8baad8f20948e2e9267f7cc4a8df04
jeremy <jeremywalker1216@gmail.com>
2022-01-30 12:20:05 -0500

    created exception package/classes

---
+++ src/main/java/com/example/superheroes/exceptions/InformationExistsException.java
@@ -0,0 +1,13 @@
+package com.example.superheroes.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class InformationExistsException extends RuntimeException {
+
+    public InformationExistsException(String message){
+        super(message);
+    }
+}
